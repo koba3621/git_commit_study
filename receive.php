5946e8568e6923c103650f7038d79828e1afb2d1
Takahiro Kobayashi <kobayashi.takah@gmail.com>
2020-11-08 22:49:05 +0900

    [add][ユーザ入力値表示画面] ユーザが入力した値を表示する画面を新規作成
    ・前画面(form.php)にて、ユーザが入力した値を表示する画面を作成しました。
    ・直接アクセスの場合には、form.phpにリダイレクトさせています。

---
+++ receive.php
@@ -0,0 +1,16 @@
+<?php
+if (!isset($_POST['user_input'])) {
+	header('Location: form.php');
+	exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>送信されたデータの表示</title>
+</head>
+<body>
+<p><?php echo htmlspecialchars($_POST['user_input'], ENT_QUOTES, 'UTF-8'); ?></p>
+</body>
+</html>
