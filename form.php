6388e559d4932f709aef907d2ae257e0111e067e
Takahiro Kobayashi <kobayashi.takah@gmail.com>
2020-11-08 22:44:49 +0900

    [add][フォーム画面] フォーム画面を新規作成
    ・後程作成する次画面(receive.php)に、ユーザが入力した文字を表示するために、
    　入力を受け付けるフォーム画面を作成しました。

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>Git Commit Message</title>
+</head>
+<body>
+<h1>好きに入力してください</h1>
+<form action="receive.php" method="post">
+<input type="text" name="user_input">
+<input type="submit" value="送信">
+</form>
+</body>
+</html>
